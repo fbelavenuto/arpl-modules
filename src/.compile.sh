eeb6c218d8a7501f5cfe4858b14a1008daaab6db
Fabio Belavenuto <belavenuto@gmail.com>
2022-08-04 16:56:29 -0300

    Organizating...

--- src/.compile.sh
+++ src/.compile.sh
@@ -2,21 +2,23 @@
 
 set -e
 
-if [ ! -d "${PWD}/${1}" ]; then
+MODULE="${1}"
+
+if [ -z "${MODULE}" ]; then
+  echo "Use: .compile.sh <module dir>"
+  exit 1
+fi
+if [ ! -d "${PWD}/${MODULE}" ]; then
   echo "Module dir not found!"
   exit 1
 fi
 
-#curl -LO "https://github.com/fbelavenuto/arpl/raw/main/PLATFORMS"
+curl -sLO "https://github.com/fbelavenuto/arpl/raw/main/PLATFORMS"
 
-for i in bromolow-3.10.108 apollolake-4.4.180 broadwell-4.4.180 broadwellnk-4.4.180 denverton-4.4.180 geminilake-4.4.180 v1000-4.4.180
-do
-  PLATFORM=`echo ${i} | cut -d'-' -f1`
-  KVER=`echo ${i} | cut -d'-' -f2`
+while read PLATFORM KVER; do
   DIR="${KVER:0:1}.x"
-  if [ -d "${PWD}/${1}/${DIR}" ]; then
-    if ! grep -q "$i" "${PWD}/${1}/.exclude" 2>/dev/null; then
-      docker run -u 1000 --rm -it -v "${PWD}/${1}/${DIR}":/input -v "${PWD}/../${i}":/output fbelavenuto/syno-compiler compile-module ${PLATFORM}
-    fi
+  [ ! -d "${PWD}/${MODULE}/${DIR}" ] && continue
+  if ! grep -q "$i" "${PWD}/${MODULE}/.exclude" 2>/dev/null; then
+    docker run -u 1000 --rm -it -v "${PWD}/${1}/${DIR}":/input -v "${PWD}/../${i}":/output fbelavenuto/syno-compiler compile-module ${PLATFORM}
   fi
-done
+done < PLATFORMS
